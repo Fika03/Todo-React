93b5db9dac8e891542c879eae396c169975c3baf
Simon <19sipe@stockholmscience.se>
2024-01-23 12:50:18 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,8 @@ import { useState } from "react";
 import "./App.css";
 import { Todos } from "./components/Todos";
 import { Todo } from "./models/Todo";
-// import { Greeting } from "./components/Greeting";
 
 function App() {
-  // const someGreeting = "Hello world";
-
   const [todos, setTodos] = useState<Todo[]>([
     new Todo("Lär dig react", false),
     new Todo("Lär dig munspel", false),
@@ -28,10 +25,6 @@ function App() {
 
   return (
     <>
-      {/* <Greeting message={someGreeting} age={10} />
-      <Greeting message="Lorem ipsum" age={10} />
-      <Greeting message="dolor sit amet" age={10} /> */}
-
       <Todos todos={todos} toggleTodo={toggleTodo} />
     </>
   );
--- src/components/Greeting.tsx
+++
@@ -1,8 +0,0 @@
-interface IGreetingProps {
-  message: string;
-  age: number;
-}
-
-export const Greeting = ({ message }: IGreetingProps) => {
-  return <h2>{message}</h2>;
-};
